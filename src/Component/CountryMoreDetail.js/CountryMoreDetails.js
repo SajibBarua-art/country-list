357962bf39e74ce42c60cbdfdc4f0c3a235aa456
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-03-07 22:03:10 +0600

    2

--- src/Component/CountryMoreDetail.js/CountryMoreDetails.js
+++ src/Component/CountryMoreDetail.js/CountryMoreDetails.js
@@ -9,14 +9,15 @@ const CountryMoreDetails = () => {
             .then(res => res.json())
             .then(data => setCountry(data));
     }, []);
-    console.log(country[0].capital, country[0]);
-    //const { capital, currencies, flag, languages, population, region, subregion } = country[0];
+    const { capital, flag, languages, population, region, subregion } = country[0] || {};
     return (
-        <div style={{ textAlign:'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <h1>Country Name: {countryName}</h1>
-            {/* 
-            <img style={{width:'400px', border:'1px solid gray'}} src={flag} alt="none"/>
-             */}
+            <img style={{ width: '400px', border: '1px solid gray' }} src={flag} alt="none" />
+            <p>Capital is: {capital}</p>
+            <p>Population is: {population}</p>
+            <p>Region is: {region}</p>
+            <p>Subregion is: {subregion}</p>
         </div>
     );
 };
